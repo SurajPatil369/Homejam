a0fc954b11dfd0e74a0b3dd5f47ff731ae022ce0
suraj patil <89598901+surajpatil510@users.noreply.github.com>
2022-02-15 21:40:26 +0530

    feat: user logout

--- controllers/user.js
+++ controllers/user.js
@@ -69,3 +69,15 @@ exports.login = asyncHandler(async (req, res, next) => {
       userId: user._id.toString(),
     });
 });
+
+//@desc   login user
+//@route  GET /api/v1/user/logout
+//@access Private
+
+exports.logout=asyncHandler(async(req,res,next)=>{
+   res.cookie('token',null,{
+    expiresIn: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+   })
+   res.status(200).json({success:true,message:'logout successful',data:{}})
+})
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const {register,login,logout}=require('../controllers/user');
-
+const isAuth=require('../middleware/is-Auth');
 router.post('/register',register);
 router.post('/login',login);
-
-// router.get('/logout',logout);
+router.get('/logout',logout);
 
 module.exports=router;
