ae477fc09cdc6632bb140b6127fbfc5c062f0786
suraj patil <89598901+surajpatil510@users.noreply.github.com>
2022-02-15 16:36:16 +0530

    initial route setup

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express=require('express');
 const app=express();
+const userRoutes=require('./routes/user');
 
 const connectDB=require('./db/connection');
 PORT=8001
 
 //database connection
 connectDB();
+
+//all routes
+app.use('/api/v1/user',userRoutes)
 
 app.listen(PORT,console.log('server started listening'))
--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,8 @@
+const router = require("express").Router();
+const {register,login,logout}=require('../controllers/user');
 
+router.post('/register',register);
+router.post('/login',login);
+router.get('/logout',logout);
+
+module.exports=router;
