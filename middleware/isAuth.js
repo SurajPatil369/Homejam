f0c904361b3f394957cdbe9c4c6a113fdcb9ebcf
suraj patil <89598901+surajpatil510@users.noreply.github.com>
2022-02-16 09:28:46 +0530

    fix: token access

---
+++ middleware/isAuth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./async");
+const generateError = require("../util/error");
+const User = require("../model/User");
+
+exports.isAuth = asyncHandler(async (req, res, next) => {
+  console.log('called this funntion')
+  // console.log(req)
+  let token =  req.get("cookie")
+  token=token.split('=')
+  token=token[1]
+  console.log(token);
+  if (!token) {
+    throw generateError("not authorized", 401);
+  }
+  try {
+    const decodedToken = jwt.verify(token, "secret_key");
+    if (!decodedToken) {
+      throw generateError("not authorized", 401);
+    }
+    const userId = decodedToken.userId;
+    const user = await User.findById(userId);
+    req.user = user;
+    next();
+  } catch (error) {
+    throw error
+
+  }
+});
